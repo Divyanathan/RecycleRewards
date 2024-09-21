e385a2d030baf3d0299ff84e79ec9744d8b61875
DivyanathanDivy <divyanathan.arul@anywhere.co>
2024-09-21 12:22:27 +0530

    Added login screen and google-services.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.recyclerewards"
+    namespace = "kotlin.kug.recyclerewards"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.recyclerewards"
+        applicationId = "kotlin.kug.recyclerewards"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
